a6520c3bc1abf6e1969a824eeb7344a0c8e77624
tianlixin <newcitysoft@163.com>
2018-03-28 15:46:27 +0800

    增加BinaryOperator函数式接口的使用案例。

--- java8-lambda/src/main/java/com/newcitysoft/study/lambda/function/Sample.java
+++ java8-lambda/src/main/java/com/newcitysoft/study/lambda/function/Sample.java
@@ -1,8 +1,10 @@
 package com.newcitysoft.study.lambda.function;
 
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.IntFunction;
 import java.util.function.IntToDoubleFunction;
+import java.util.function.UnaryOperator;
 
 /**
  * IntFunction<R>, IntToDoubleFunction,
@@ -42,9 +44,25 @@ public class Sample {
         System.out.println(function.apply(123));
     }
 
+    private static void testBinaryOperator() {
+        // 3
+        BinaryOperator<Integer> sum = (a, b) -> a + b;
+        Integer res = sum.apply(1, 2);
+        System.out.println(res);
+
+        BinaryOperator<Function<Integer, Integer>> compose = (f, g) -> x -> g.apply(f.apply(x));
+
+        UnaryOperator<Integer> add2    = n -> n + 1;
+        UnaryOperator<String>                     concat1 = s -> s + 1;
+        Function<Integer, UnaryOperator<Integer>> sum2    = x -> y -> x + y;
+        UnaryOperator<Integer>                    sum10   = sum2.apply(10);
+
+    }
+
     public static void main(String[] args) {
         testFunction();
         testIntToDoubleFunction();
         testIntFunction();
+        testBinaryOperator();
     }
 }
