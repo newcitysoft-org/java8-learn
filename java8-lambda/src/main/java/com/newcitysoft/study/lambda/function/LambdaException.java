a57d85136e26e4a13a93939ca92a704ff5e40273
tianlixin <newcitysoft@163.com>
2018-03-28 16:11:03 +0800

    增加lambda的捕获异常。

---
+++ java8-lambda/src/main/java/com/newcitysoft/study/lambda/function/LambdaException.java
@@ -0,0 +1,30 @@
+package com.newcitysoft.study.lambda.function;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.function.Function;
+
+/**
+ * lambda 中有异常
+ * <p>
+ * 任何函数式接口都不允许抛出受检异常
+ * <p>
+ * sf上这个问题的一些讨论：https://stackoverflow.com/questions/18198176/java-8-lambda-function-that-throws-exception
+ *
+ * @author biezhi
+ * @date 2018/2/10
+ */
+public class LambdaException {
+
+    public static void main(String[] args) {
+        Function<BufferedReader, String> f = b -> {
+            try {
+                return b.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+
+}
